f10325e61bd6fd57f553013cd49115bc654ca264
JollyPerson <18639773+JollyPerson@users.noreply.github.com>
2022-12-14 11:55:16 +0000

    Skeleton

---
+++ algos/sjf.py
@@ -0,0 +1,22 @@
+class ShortestJobFirst:
+
+    input_queue = [[0] * 4 for i in range(3)]
+
+
+    def __init__(self):
+        pass
+
+    def add_process(self,orocess_id: str, arrival_time: int, burst_time: int):
+        pass
+
+    def remove_process(self, process_id: str) -> bool:
+        return True
+
+    def calculate_shortest_job_first(self):
+        pass
+
+    def get_total_time_remaining(self, instant: int)-> int:
+        pass
+
+    def to_start_stop() -> [][]:
+        return [][]
